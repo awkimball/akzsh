f7b31f2123aec9ad19611fee09f0f8243cb9beeb
Addison Kimball <addison@akimball.com>
2022-01-20 13:23:57 -0600

    fix file copy location

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Copying akzsh.zsh-theme to ~/.oh-my-zsh/custom/themes/
-cp akzsh.zsh-theme sed -i '' -E 's|#?ZSH_THEME="[^[:space:]]*"|ZSH_THEME="akzsh"|'
+cp akzsh.zsh-theme ~/.oh-my-zsh/custom/themes/
 echo done!
 echo Setting theme in .zshrc
 sed -i '' -E 's|ZSH_THEME="[^[:space:]]*"|ZSH_THEME="akzsh"|' ~/.zshrc
